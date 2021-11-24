7b74193a7b9bd17b79b68896d426bde2b1aab095
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-11-24 13:56:31 +0700

    add tags and category to guest index

--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -6,6 +6,16 @@
   <div class="blog-post">
     <h2 class="blog-post-title"><a href="{{route('post.show', $post['slug'])}}">{{$post['title']}}</a></h2>
     <p class="blog-post-meta">{{$post->updated_at->diffForHumans()}} by <a href="#">Chris</a></p>
+    @if ($post['tags'])
+      <p>
+        @foreach ($post['tags'] as $tag)
+          <a href="{{route('tag.show', $tag['slug'])}}"><span class="badge badge-pill badge-primary">{{$tag['name']}}</span></a>
+        @endforeach
+      </p>
+    @endif
+    @if ($post['category'])
+      <h5>Category: <a href="{{route('category.show', $post->category['slug'])}}">{{$post['category']['name']}}</a></h5>  
+    @endif
     <p>{{$post['content']}}</p>
   </div><!-- /.blog-post -->
   @endforeach
